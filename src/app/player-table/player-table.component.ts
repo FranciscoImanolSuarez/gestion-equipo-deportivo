6bf50b34e1490a450934f3fa32f40e18731ac71f
FranciscoImanolSuarez <imanolsuarez98@gmail.com>
2020-10-27 12:59:12 -0300

    add delete players

--- src/app/player-table/player-table.component.ts
+++ src/app/player-table/player-table.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { Player } from '../interfaces/player';
 import { PlayerService } from '../services/player.service';
+import { TeamService } from '../services/team.service';
 
 @Component({
   selector: 'app-player-table',
   templateUrl: './player-table.component.html',
-  styleUrls: ['./player-table.component.scss'],
+  styleUrls: ['./player-table.component.scss']
 })
 export class PlayerTableComponent implements OnInit {
   public players$: Observable<Player[]>;
   public selectedPlayer: Player;
   public showModal = false;
-  constructor(private playerService: PlayerService) {}
+  constructor(private playerService: PlayerService, private teamService: TeamService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.players$ = this.playerService.getPlayers();
   }
 
@@ -23,6 +25,33 @@ export class PlayerTableComponent implements OnInit {
     this.selectedPlayer = null;
     setTimeout(() => {
       window.location.replace('#open-modal');
-    }, 0);
+    });
+  }
+
+  editPlayer(player: Player) {
+    this.selectedPlayer = { ...player };
+    this.showModal = true;
+    setTimeout(() => {
+      window.location.replace('#open-modal');
+    });
+  }
+
+  deletePlayer(player: Player) {
+    this.teamService
+      .getTeams()
+      .pipe(take(1))
+      .subscribe(teams => {
+        const moddifiedPlayers = teams[0].players ? teams[0].players.filter((p: any) => p.key !== player.$key) : teams[0].players;
+        const formattedTeam = {
+          ...teams[0],
+          players: [...moddifiedPlayers]
+        };
+        this.playerService.delete(player.$key);
+        this.teamService.editTeam(formattedTeam);
+      });
+  }
+
+  closeDialog() {
+    this.showModal = false;
   }
 }
